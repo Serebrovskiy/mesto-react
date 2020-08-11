655e0f31a75777fb877f145052e9586e36a5c759
Aleksandr Serebrovskiy <aleksandr-web@yandex.ru>
2020-08-11 16:37:57 +0400

    refactoring

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card';
 import { api } from '../utils/api';
 
-function Main({onEditAvatar, onEditProfile, onAddCard, onCardClick}) {
+function Main({ onEditAvatar, onEditProfile, onAddCard, onCardClick }) {
   const [userName, setUserName] = React.useState('');
   const [userDescription, setUserDescription] = React.useState('');
   const [userAvatar, setUserAvatar] = React.useState('');
@@ -10,38 +10,29 @@ function Main({onEditAvatar, onEditProfile, onAddCard, onCardClick}) {
   const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
-    api.getProfile().then(res => {
-      setUserName(
-        res.name
-      );
-      setUserDescription(
-        res.about
-      );
-      setUserAvatar(
-        res.avatar
-      );
-    }).catch((err) => console.log(err));
-  });
-
-  React.useEffect(() => {
-    api.getInitialCards().then((res) => {
-      setCards(res.map(item => ({
-        id: item._id,
-        likes: item.likes,
-        name: item.name,
-        src: item.link
-      })));
-    }).catch((err) => console.log(err));
+    Promise.all([api.getProfile(), api.getInitialCards()])
+      .then((res) => {
+        setUserName(res[0].name);
+        setUserDescription(res[0].about);
+        setUserAvatar(res[0].avatar);
+        setCards(res[1].map(item => ({
+          id: item._id,
+          likes: item.likes,
+          name: item.name,
+          src: item.link
+        })));
+      })
+      .catch((err) => console.error(err));
   }, []);
 
   return (
     <>
       <section className="profile">
         <div className="profile__avatar-container" onClick={onEditAvatar}>
-          <img className="profile__avatar" src={userAvatar} alt="Аватар" /> 
+          <img className="profile__avatar" src={userAvatar} alt="Аватар" />
         </div>
         <div className="profile__name-block">
-          <h2 className="profile__name">{userName}</h2>  {/* Жак-Ив Кусто */}
+          <h2 className="profile__name">{userName}</h2>
           <button type="button" className="profile__edit" onClick={onEditProfile}></button>
           <p className="profile__profession">{userDescription}</p>
         </div>
